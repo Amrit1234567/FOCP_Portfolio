d0332391dd210f73d94be4d0de41f791514082bb
Amrit <sharma.amrit9861@gmail.com>
2024-01-31 15:03:19 +0545

    Thrid commit

--- task2.py
+++ task2.py
@@ -5,9 +5,9 @@ def display_result(ours, theirs, length):
     print(f"Cat Visits {ours}")
     print(f"Other Cats {theirs}")
     print(f"\nTotal Time in House: {sum(length)//60} Hours, {sum(length)%60} Minute") if sum(length)%60 <=1 else print(f"\nTotal Time in House: {sum(length)//60} Hours, {sum(length)%60} Minutes")
-    print(f"\nAverage Visit Length: {sum(length)//len(length)} Minutes")
-    print(f"Longest Visit: {max(length)} Minutes")
-    print(f"Shortest Visit: {min(length)} Minute") if min(length) <= 1 else print(f"Shortest Visit: {min(length)} Minutes")
+    print(f"\n{'Average Visit Length':20}: {sum(length)//len(length)} Minutes")
+    print(f"{'Longest Visit':20}: {max(length)} Minutes")
+    print(f"{'Shortest Visit':20}: {min(length)} Minute") if min(length) <= 1 else print(f"{'Shortest Visit':20}: {min(length)} Minutes")
 
 
 if __name__ == "__main__":
